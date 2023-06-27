2ea270fb842194d3e2e0a023bb6740571aa06950
ggeonWoo <130333426+ggeonWoo@users.noreply.github.com>
2023-06-27 15:23:31 +0900

    JS-Home

---
+++ SRC/MAIN.js
@@ -0,0 +1,17 @@
+// HEADER에 페이지 아래로 스크롤시 다크 스타일링으로 적용
+const header=document.querySelector('.header');
+const headerHeight= header.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    
+    if(window.scrollY>headerHeight){
+        header.classList.add('header--dark');
+    }else{
+        header.classList.remove('header--dark');
+    }
+});
+
+const home__container=document.querySelector('.home__container');
+const homeHeight= home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity=1-window.scrollY/homeHeight
+});
