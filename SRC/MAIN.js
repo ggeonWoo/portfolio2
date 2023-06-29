5654de72c422c55d2fa417ee49caf79ab7439fd6
ggeonWoo <130333426+ggeonWoo@users.noreply.github.com>
2023-06-29 17:02:45 +0900

    JS-project

--- SRC/MAIN.js
+++ SRC/MAIN.js
@@ -36,3 +36,43 @@ navbarToggle.addEventListener('click',()=>{
 navbarMenu.addEventListener('click',()=>{
     navbarMenu.classList.remove('open')
 })
+
+
+//프로젝트 필터링 관련 로직 처리
+
+const categories =document.querySelector('.categories')
+const projects=document.querySelectorAll('.project')
+const projectsContainer=document.querySelector('.projects')
+categories.addEventListener('click',(event)=>{
+const filter= event.target.dataset.category;
+if(filter==null){
+    return;
+}
+handleActiveSelection(event.target)
+filterContainer(filter)
+
+
+});
+
+function handleActiveSelection(target){
+    //active 메뉴를 재설정
+const active =document.querySelector('.category--selected')
+active.classList.remove('category--selected');
+target.classList.add('category--selected');
+
+}
+
+function filterContainer(filter){
+    // 프로젝트 필터링
+projectsContainer.classList.add('anim-out')
+projects.forEach((project)=>{
+    if(filter==='all'||filter===project.dataset.type){
+        project.style.display='block';
+    }else{
+        project.style.display='none';
+    }
+    });
+    setTimeout(()=>{
+        projectsContainer.classList.remove('anim-out')
+    },250)
+}
